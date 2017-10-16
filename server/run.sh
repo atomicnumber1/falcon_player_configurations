a8ef377a6cb569df8bba3912f3f70fd3aaff5d2a
atomicnumber1 <0xproton@gmail.com>
2017-10-17 00:14:40 +0530

    Added run script

---
+++ server/run.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+SRCDIR="${HOME}/code/falcon_player_configurations/server"
+VENV="falcon_player_controller"
+
+source ~/.pyenv/versions/falcon_player_controller/bin/activate
+cd $SRCDIR
+nohup python app.py &
