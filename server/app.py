1269aceedf715d9aa7a4236a2433470d25415883
atomicnumber1 <0xproton@gmail.com>
2017-10-16 22:11:48 +0530

    bug fixes

--- server/app.py
+++ server/app.py
@@ -35,26 +35,28 @@ parser = reqparse.RequestParser()
 parser.add_argument('action')
 parser.add_argument('playlist')
 
-# info
+# Info
 # shows info about supported actions
-class info():
+class Info(Resource):
     def get(self):
         return actions_supported
 
-# do
+# Do
 # performs the requested actions
-class do(Resource):
+class Do(Resource):
 
-    actions = {
-        'play': lambda x: self.play_palylist(x),
-        'pause': lambda x: self.pause_palylist(x),
-        'stop': lambda x: self.stop_playlist(),
-        'reboot': lambda x: self.reboot(),
-        'shutdown': lambda x: self.shutdown(),
-    }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.actions = {
+            'play': lambda x: self.play_palylist(x),
+            'pause': lambda x: self.pause_palylist(x),
+            'stop': lambda x: self.stop_playlist(),
+            'reboot': lambda x: self.reboot(),
+            'shutdown': lambda x: self.shutdown(),
+        }
 
     def get(self, action, playlist=None):
-        return choices.get(action)(playlist)
+        return self.actions.get(action)(playlist)
 
     def play_playlist(self, playlist):
         if not playlist:
@@ -62,59 +64,58 @@ class do(Resource):
 
         if playlist == 'audio':
             try:
-                subprocess.check_call(['bash', '%s/StartVideoPlaylist.sh'.format(SCRIPTS_DIR)])
+                subprocess.check_call(['bash', '{}/StartVideoPlaylist.sh'.format(SCRIPTS_DIR)])
                 return ('Success! Playing Audio Playlist')
             except subprocess.CalledProcessError as e:
-                print('Error Occurred during execution of StartAudioPlaylist:\n%s'.format(e))
+                print('Error Occurred during execution of StartAudioPlaylist:\n{}'.format(e))
                 return ('Ooops! Something\'s Wrong')
         elif playlist == 'video':
             try:
-                subprocess.check_call(['bash', '%s/StartVideoPlaylist.sh'.format(SCRIPTS_DIR)])
+                subprocess.check_call(['bash', '{}/StartVideoPlaylist.sh'.format(SCRIPTS_DIR)])
                 return ('Success! Playing Video Playlist')
             except subprocess.CalledProcessError as e:
-                print('Error Occurred during execution of StartVideoPlaylist:\n%s'.format(e))
+                print('Error Occurred during execution of StartVideoPlaylist:\n{}'.format(e))
                 return ('Ooops! Something\'s Wrong')
 
-    def pause_playlist(self, playlist):
+    def pause_playlist(self, **kwargs):
         return ('Oops! Not Implemented yet')
 
     def stop_playlist(self, **kwargs):
         try:
-            subprocess.check_call(['bash', '%s/stopfpp.sh'.format(SCRIPTS_DIR)])
+            subprocess.check_call(['bash', '{}/stopfpp.sh'.format(SCRIPTS_DIR)])
             return ('Success! Stopped Playlist')
         except subprocess.CalledProcessError as e:
-            print('Error Occurred during execution of stopfpp:\n%s'.format(e))
+            print('Error Occurred during execution of stopfpp:\n{}'.format(e))
             return ('Ooops! Something\'s Wrong')
 
     def reboot(self, **kwargs):
         try:
-            return ('Rebooting Pi!')
-            subprocess.check_call(['shutdown', '-h', 'now'])
-        except subprocess.CalledProcessError as e:
-            print('Error Occurred while Shutting down execution of stopfpp:\n%s'.format(e))
+            subprocess.Popen(['bash', '{}/reboot.sh'.format(SCRIPTS_DIR)])
+            return ('Rebooting Pi in 10 secs!')
+        except Exception as e:
+            print('Error Occurred while Rebooting:\n{}'.format(e))
             return ('Ooops! Something\'s Wrong')
 
     def shutdown(self, **kwargs):
         try:
-            return ('Shutting down Pi!')
-            subprocess.check_call(['shutdown', '-h', 'now'])
-        except subprocess.CalledProcessError as e:
-            print('Error Occurred while Shutting down execution of stopfpp:\n%s'.format(e))
+            subprocess.Popen(['bash', '{}/shutdown.sh'.format(SCRIPTS_DIR)])
+            return ('Shutting down Pi in 10 secs!')
+        except Exception as e:
+            print('Error Occurred while Shutting down:\n{}'.format(e))
             return ('Ooops! Something\'s Wrong')
 
 ##
 ## Actually setup the Api resource routing here
 ##
-api.add_resource(info, '/')
-api.add_resource(do, '/<action>/')
-api.add_resource(do, '/<action>/<playlist>')
+api.add_resource(Info, '/')
+api.add_resource(Do, '/<action>/<playlist>')
 
 
 if __name__ == "__main__":
     app.secret_key = 'C(@WDiuTP796%yZH*zcfOssgvdifhYmZ'
 
     from gevent.wsgi import WSGIServer
-    address = ("localhost", 2017)
+    address = ("0.0.0.0", 2017)
     server = WSGIServer(address, app,
         log=my_logger, error_log=my_logger)
     try:
