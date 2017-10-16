605e19b2cc350e278c8aad8c6a75277dc3124b02
atomicnumber1 <0xproton@gmail.com>
2017-10-16 19:12:40 +0530

    Initial script for copying media

---
+++ udev/copy_flash_daemon.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TARGET_FOLDER=/home/fpp/media
+SOURCE=/media/usb
+
+while true
+do
+    if lsblk | grep -i "usb"
+    then
+        # for usb in /media/*; do
+        cp -rf $SOURCE/sequences/ $TARGET_FOLDER &> /dev/null;
+        cp -rf $SOURCE/music/ $TARGET_FOLDER &> /dev/null;
+        cp -rf $SOURCE/videos/ $TARGET_FOLDER &> /dev/null;
+        # done
+    fi
+    sleep 15;
+done
