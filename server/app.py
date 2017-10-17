905a944f3923af7afd110d252187036a9aadf881
atomicnumber1 <0xproton@gmail.com>
2017-10-17 09:41:02 +0530

    Return Proper json response

--- server/app.py
+++ server/app.py
@@ -27,16 +27,33 @@ handler = logging.handlers.RotatingFileHandler(
 
 logger.addHandler(handler)
 
-app = Flask(__name__)
-api = Api(app)
+FPPApp = Flask(__name__)
+FPPApi = Api(FPPApp)
 
-actions_supported = ['start', 'pause', 'stop', 'reboot', 'shutdown']
+ACTIONS_SUPPORTED = [
+    'start',
+    'pause',
+    'stop',
+    'reboot',
+    'shutdown'
+]
+
+STATUS_CODES = {
+    'Success': 0,
+    'PlayError': 1,
+    'PauseError': 2,
+    'StopError': 3,
+    'RebootError': 4,
+    'ShutdownError': 5,
+}
+def gen_response(status, msg):
+    return {'status': status, 'msg': msg}
 
 # Info
 # shows info about supported actions
 class Info(Resource):
     def get(self):
-        return actions_supported
+        return gen_response(STATUS_CODES['Success'], ACTIONS_SUPPORTED)
 
 # Playlist
 # performs the requested playlist actions
@@ -49,7 +66,7 @@ class Playlist(Resource):
             'pause': lambda x: self.pause_playlist(),
             'stop': lambda x: self.stop_playlist(),
         }
-        self.default = lambda x: list(self.actions.keys())
+        self.default = lambda x: gen_response(STATUS_CODES['Success'], list(self.actions.keys()))
 
     def get(self, action, playlist):
         return self.actions.get(action, self.default)(playlist)
@@ -59,30 +76,31 @@ class Playlist(Resource):
         if playlist == 'audio':
             try:
                 subprocess.check_call(['bash', '{}/StartAudioPlaylist.sh'.format(SCRIPTS_DIR)])
-                return ('[0] Success! Playing Audio Playlist')
+                return gen_response(STATUS_CODES['Success'], 'Playing Audio Playlist')
             except subprocess.CalledProcessError as e:
                 logger.error('Error Occurred during execution of StartAudioPlaylist:\n{}'.format(e))
-                return ('[1] Ooops! Something\'s Wrong')
+                return gen_response(STATUS_CODES['PlayError'], 'Ooops! Something\'s Wrong')
+
         elif playlist == 'video':
             try:
                 subprocess.check_call(['bash', '{}/StartVideoPlaylist.sh'.format(SCRIPTS_DIR)])
-                return ('[0] Success! Playing Video Playlist')
+                return gen_response(STATUS_CODES['Success'], 'Playing Video Playlist')
             except subprocess.CalledProcessError as e:
                 logger.error('Error Occurred during execution of StartVideoPlaylist:\n{}'.format(e))
-                return ('[1] Ooops! Something\'s Wrong')
-        return ('[1] Yikes! No Such Playlist!')
+                return gen_response(STATUS_CODES['PlayError'], 'Ooops! Something\'s Wrong')
+        return gen_response(STATUS_CODES['PlayError'], 'Yikes! No Such Playlist!')
 
     def pause_playlist(self, **kwargs):
         logger.warning("[pause_playlist] Not Implemented function.")
-        return ('[2] Oops! Not Implemented yet')
+        return gen_response(STATUS_CODES['PauseError'], 'Oops! Not Implemented yet')
 
     def stop_playlist(self, **kwargs):
         try:
             subprocess.Popen(['bash', '{}/stopfpp.sh'.format(SCRIPTS_DIR)])
-            return ('[0] Success! Stopped Playlist')
+            return gen_response(STATUS_CODES['Success'], 'Stopped Playlist')
         except Exception as e:
             logger.error('Error Occurred during execution of stopfpp:\n{}'.format(e))
-            return ('[3] Ooops! Something\'s Wrong')
+            return gen_response(STATUS_CODES['StopError'], 'Ooops! Something\'s Wrong')
 
 class System(Resource):
 
@@ -92,7 +110,7 @@ class System(Resource):
             'reboot': lambda: self.reboot(),
             'shutdown': lambda: self.shutdown(),
         }
-        self.default = lambda: list(self.actions.keys())
+        self.default = lambda: gen_response(STATUS_CODES['Success'], list(self.actions.keys()))
 
     def get(self, action):
         return self.actions.get(action, self.default)()
@@ -100,33 +118,33 @@ class System(Resource):
     def reboot(self):
         try:
             subprocess.Popen(['bash', '{}/reboot.sh'.format(SCRIPTS_DIR)])
-            return ('[0] Rebooting Pi in 10 secs!')
+            return gen_response(STATUS_CODES['Success'], 'Rebooting Pi in 10 secs!')
         except Exception as e:
             logger.error('Error Occurred while Rebooting:\n{}'.format(e))
-            return ('[4] Ooops! Something\'s Wrong')
+            return gen_response(STATUS_CODES['RebootError'], 'Ooops! Something\'s Wrong')
 
     def shutdown(self):
         try:
             subprocess.Popen(['bash', '{}/shutdown.sh'.format(SCRIPTS_DIR)])
-            return ('[0] Shutting down Pi in 10 secs!')
+            return gen_response(STATUS_CODES['Success'], 'Shutting down Pi in 10 secs!')
         except Exception as e:
             logger.error('Error Occurred while Shutting down:\n{}'.format(e))
-            return ('[5] Ooops! Something\'s Wrong')
+            return gen_response(STATUS_CODES['ShutdownError'], 'Ooops! Something\'s Wrong')
 
 ##
 ## Actually setup the Api resource routing here
 ##
-api.add_resource(Info, '/')
-api.add_resource(System, '/<action>')
-api.add_resource(Playlist, '/<action>/<playlist>')
+FPPApi.add_resource(Info, '/')
+FPPApi.add_resource(System, '/<action>')
+FPPApi.add_resource(Playlist, '/<action>/<playlist>')
 
 
 if __name__ == "__main__":
-    app.secret_key = 'C(@WDiuTP796%yZH*zcfOssgvdifhYmZ'
+    FPPApp.secret_key = 'C(@WDiuTP796%yZH*zcfOssgvdifhYmZ'
 
     from gevent.wsgi import WSGIServer
     address = ("0.0.0.0", 2017)
-    server = WSGIServer(address, app,
+    server = WSGIServer(address, FPPApp,
         log=logger, error_log=logger)
     try:
         logger.info("Server running on port %s:%d. Ctrl+C to quit" % address)
