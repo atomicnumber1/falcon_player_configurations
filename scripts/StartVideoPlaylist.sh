846e19827cfbcd44f11bb0345829ad9df587cef6
atomicnumber1 <0xproton@gmail.com>
2017-10-20 00:23:27 +0530

    Fixed multiple videos playing bug

--- scripts/StartVideoPlaylist.sh
+++ scripts/StartVideoPlaylist.sh
@@ -7,15 +7,15 @@
 
 ###################################################################
 # Set some environment variables
-. /opt/fpp/scripts/common
+# . /opt/fpp/scripts/common
 
 FPP="/opt/fpp/bin.pi/fpp"
-FORCEAUDIO="-o local"
-VOLUME=75
-# 75% FPP volume is 87.5% amixer which is around 0dB in omxplayer
-DB=$(echo "((50 + (${VOLUME}/2)) - 87.5) / 2.5 * 3" | bc)
 PLAYLISTNAME="video"
-VIDEOFILE=$(ls ${MEDIADIR}/videos/)
+# FORCEAUDIO="-o local"
+# VOLUME=75
+# # 75% FPP volume is 87.5% amixer which is around 0dB in omxplayer
+# DB=$(echo "((50 + (${VOLUME}/2)) - 87.5) / 2.5 * 3" | bc)
+# VIDEOFILE=$(ls ${MEDIADIR}/videos/)
 
 # Stop current playlist
 $FPP -d
@@ -26,5 +26,5 @@ sudo killall -9 omxplayer.bin
 # Start playlist
 $FPP -P "${PLAYLISTNAME}"
 
-# Start video
-nohup sudo -u fpp /usr/bin/omxplayer ${FORCEAUDIO} --no-keys --vol ${DB}00 "${MEDIADIR}/videos/${VIDEOFILE}" &> /dev/null &
+# Start video (No need)
+# nohup sudo -u fpp /usr/bin/omxplayer ${FORCEAUDIO} --no-keys --vol ${DB}00 "${MEDIADIR}/videos/${VIDEOFILE}" &> /dev/null &
