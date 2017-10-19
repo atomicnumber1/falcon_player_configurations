61f72bf8d8d1b66a208f45d67abbc2defe56296a
atomicnumber1 <0xproton@gmail.com>
2017-10-19 20:22:14 +0530

    Stupid double quotes!

--- scripts/update_ssid.sh
+++ scripts/update_ssid.sh
@@ -3,7 +3,7 @@
 SRC=/home/fpp/code/falcon_player_configurations
 FILENAME=$SRC/configurations/ssid_updated.txt
 
-sudo sed -i.bak 's/ssid=.*/ssid=$1/g' /etc/hostapd/hostapd.conf
+sudo sed -i.bak "s/ssid=.*/ssid=$1/g" /etc/hostapd/hostapd.conf
 echo 1 > $FILENAME
 
 exec bash $SRC/scripts/reboot.sh &
